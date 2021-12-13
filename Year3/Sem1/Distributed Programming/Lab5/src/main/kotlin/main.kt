001ea76713316d58fa0df4e4d1d8d1c8a231d3b3
Victor Sera <Victor.Sera@cloudflight.io>
2021-12-14 00:17:57 +0200

    Implement sequential karatsuba ppd 5

--- Year3/Sem1/Distributed Programming/Lab5/src/main/kotlin/main.kt
+++ Year3/Sem1/Distributed Programming/Lab5/src/main/kotlin/main.kt
@@ -1,14 +1,18 @@
 import strategy.MultiplicationStrategy
+import strategy.impl.ParallelOn2Multiplication
+import strategy.impl.SequentialKaratsubaMultiplication
 import strategy.impl.SequentialOn2Multiplication
 import kotlin.system.measureTimeMillis
 
-fun main(args: Array<String>)
+fun main()
 {
     val p1 = Polynomial(mapOf(10 to 2, 5 to 3, 4 to 4, 2 to 2, 1 to 5, 0 to 5))
     val p2 = Polynomial(mapOf(3 to 2, 2 to 7, 1 to 2))
     println("Benchmarking multiplication:\n$p1 *\n$p2")
 
     benchmark(SequentialOn2Multiplication(), p1, p2)
+    benchmark(ParallelOn2Multiplication(), p1, p2)
+    benchmark(SequentialKaratsubaMultiplication(), p1, p2)
 }
 
 fun benchmark(strategy: MultiplicationStrategy, p1: Polynomial, p2: Polynomial) {
--- Year3/Sem1/Distributed Programming/Lab5/src/main/kotlin/strategy/impl/ParallelOn2Multiplication.kt
+++ Year3/Sem1/Distributed Programming/Lab5/src/main/kotlin/strategy/impl/ParallelOn2Multiplication.kt
@@ -1,13 +1,40 @@
 package strategy.impl
 
 import Polynomial
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.async
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import strategy.MultiplicationStrategy
+import java.util.concurrent.Executors
 
 class ParallelOn2Multiplication: MultiplicationStrategy
 {
     override fun multiply(p1: Polynomial, p2: Polynomial): Polynomial
     {
-        return p1.coefficientMap.map { (power, coefficient) -> p2.multiplyByElement(coefficient, power) }
-            .reduce { acc, polynomial -> acc.add(polynomial) }
+        val pool = Executors.newFixedThreadPool(2).asCoroutineDispatcher()
+        var result = Polynomial()
+        val mutex = Mutex()
+
+        runBlocking {
+            p1.coefficientMap.forEach { (power, coefficient) ->
+                launch (pool) {
+                    val partialSum = p2.multiplyByElement(coefficient, power)
+
+                    mutex.withLock {
+                        result = result.add(partialSum)
+                    }
+                }
+            }
+        }
+
+        return result
+    }
+
+    override fun toString(): String {
+        return "Parallel O(n\u00B2)"
     }
 }
